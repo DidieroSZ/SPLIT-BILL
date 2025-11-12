d8004a29a274bad91fec2c7b6845fa044a536dd0
DidieroSZ <octaviosz2003@hotmail.com>
2025-11-12 15:36:52 -0600

    feat(js): ADDED API DICEBEAR

--- src/components/people-component/people-component.js
+++ src/components/people-component/people-component.js
@@ -6,6 +6,11 @@ import { unsafeCSS } from 'lit-element';
 import { unsafeHTML } from 'lit-html/directives/unsafe-html.js';
 import { svgIcons } from '../../utils/icons.js'
 
+import { createAvatar } from '@dicebear/core'; // <-- GENERAL STYLES
+import { glass } from '@dicebear/collection'; // <-- GENERAL STYLES
+
+
+
 export class PeopleComponent extends LitElement{
 
     createRenderRoot() {
@@ -13,14 +18,19 @@ export class PeopleComponent extends LitElement{
     }
 
     static properties = {
+        margen: {type: Number },
         valor: {type: Number },
         avatares: {type: Array },
     }
     constructor(){
         super();
+        this.margen = 15;
         this.valor = 2;
         this.avatares = [];
     }
+
+    
+    
 
     /* static styles = [
         css` ${unsafeCSS(bootstrap)} `,
@@ -30,13 +40,15 @@ export class PeopleComponent extends LitElement{
     render(){
         return html`
             <article class="general--container people--container d-flexx d-row gap-3 p-3 rounded-3 border border-1">
+                <div class="header--container"><h3 class="m-0 text-center">Dividir entre personas:</h3></div>
                 <span @click=${this._btnClick} data-type="min" class="btn--circle d-flexx border trans"> ${unsafeHTML(svgIcons.minus)} </span>
                 <div class="number--container px-3">
                     <p id="people--counter">2</p>
                 </div>
                 <span @click=${this._btnClick} data-type="sum" class="btn--circle d-flexx border trans"> ${unsafeHTML(svgIcons.plus)} </span>
-                <div class="avatar--container d-flexx gap-2">
+                <div class="avatar--container d-flexx" style="margin-left: ${this.margen}px;">
                     ${this._renderAvatares()}
+                    
                 </div>
             </article>
         `;
@@ -54,22 +66,42 @@ export class PeopleComponent extends LitElement{
             people.textContent = parseInt(this.valor - 1);
             this.valor = this.valor - 1;
         }
+
+        this.margen = 15 * this.valor;
         this._eventLauncher(this.valor);
     }
+
 
     _renderAvatares(){
         const v = this.valor;
         this.avatares = [];
+        
         for (let i = 0; i < v; i++) {
+            const svg = this._createAvatar();
+            const pos = 15 * i;
+
             this.avatares.push(html `
-                <span class="btn--circle avatar d-flexx border trans">
-                    <img src="https://avatar.iran.liara.run/public">
+                <span class="btn--circle avatar d-flexx border trans" style="transform: translateX(-${pos}px);">
+                    ${unsafeHTML(svg)}
+                    <!-- <img src="https://avatar.iran.liara.run/public"> -->
                 </span>
             `);
         }
         return html`${this.avatares}`;
     }
 
+
+    _createAvatar(){
+        let min = 1;
+        let max = 500;
+        const randNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+
+        const avatar = createAvatar(glass, {
+            seed: randNumber,
+        });
+        const svgAvatar = avatar.toString();
+        return svgAvatar; 
+    }
      _eventLauncher(v){
          this.dispatchEvent(
             new CustomEvent('people-set', {
